e7c3b51456058cd050fac0e74f1868ce8d83237e
abuammar003 <ammarshah1252@gmail.com>
2026-09-22 17:45:32 +0530

    Updated Detail Page's Image Problem

--- FE/src/pages/detail.jsx
+++ FE/src/pages/detail.jsx
@@ -31,14 +31,14 @@ const ShoppingDetail = () => {
       <div className="relative border">
         
         <span className="absolute top-4 right-4">
-            <RxCrossCircled className="text-xl text-black cursor-pointer" onClick={goBack}/>
+            <RxCrossCircled className="text-2xl text-black cursor-pointer" onClick={goBack}/>
         </span>
 
         <div className="flex justify-center p-2 mb-6 items-center">
          
           <div className="flex flex-col justify-center items-center text-center">
-            <img src={product.thumbnail} alt={product.title} className="max-sm:h-44 sm:h-52 items-center" />
-            <h2 className="max-sm:text-xl sm:text-2xl mt-4">{product.title}</h2>
+            <img src={product.image} alt={product.name} className="max-sm:h-44 sm:h-52 items-center" />
+            <h2 className="max-sm:text-xl sm:text-2xl mt-4">{product.name}</h2>
             <p className="max-sm:text-lg sm:text-xl text-gray-600 mt-2">{product.description}</p>
             <div className="mt-2">
               <span className="text-2xl font-bold">${product.price}</span>
